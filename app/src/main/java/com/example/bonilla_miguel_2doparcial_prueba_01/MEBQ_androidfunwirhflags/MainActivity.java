bf3d55b2ab1cfbe6a4cdf5232225c05ceb1a1a16
Miguel Enrique Bonilla Quishpe <64868918+MiguelBonilla814@users.noreply.github.com>
2022-01-10 18:46:16 -0500

    18:46

--- app/src/main/java/com/example/bonilla_miguel_2doparcial_prueba_01/MEBQ_androidfunwirhflags/MainActivity.java
+++ app/src/main/java/com/example/bonilla_miguel_2doparcial_prueba_01/MEBQ_androidfunwirhflags/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.bonilla_miguel_2doparcial_prueba_01;
+package com.example.bonilla_miguel_2doparcial_prueba_01.MEBQ_androidfunwirhflags;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.bonilla_miguel_2doparcial_prueba_01.R;
 
 public class MainActivity extends AppCompatActivity {
 
